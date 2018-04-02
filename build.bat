9b1a72471d657d5ef17ff5b663e00eb23fff85b8
kartman3103 <pasha3103@bk.ru>
2018-04-02 17:19:11 +0300

    init commit

---
+++ build.bat
@@ -0,0 +1,1 @@
+mvn -P mvnrepository -e clean package
---
+++ src/test/java/backoffice/tests/InitTest.java
@@ -0,0 +1,34 @@
+package backoffice.tests;
+
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.Charset;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class InitTest
+{
+    @Test
+    public void pingTest()
+    {
+
+        try
+        {
+            Request request = Request.Post("http://localhost:8585/availableLeagues");
+            HttpResponse response = request.execute().returnResponse();
+
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            response.getEntity().writeTo(output);
+
+            String content = output.toString(Charset.defaultCharset().name());
+
+            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+            Assert.assertTrue(!content.isEmpty());
+        }
+        catch (Exception ex) { }
+    }
+}
