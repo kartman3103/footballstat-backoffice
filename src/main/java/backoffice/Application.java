d08a6d935e3bdf4f52374f1282e4520782d10018
kartman3103 <pasha3103@bk.ru>
2018-04-06 14:37:45 +0300

    remove redudant annotation 2

--- src/main/java/backoffice/Application.java
+++ src/main/java/backoffice/Application.java
@@ -4,11 +4,9 @@ package backoffice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-@Configuration
 @EnableConfigurationProperties
 public class Application
 {
